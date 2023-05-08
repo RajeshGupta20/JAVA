81fc95106ed3e9bc147bdb48063e27bbd7f71c1d
RajeshGupta20 <rajgupta2021252gmail.com>
2023-05-08 22:58:36 +0530

    variable

---
+++ DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+    public static void main(String[] args) {
+// Data types in java
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+        byte num1= 1;
+        boolean statement =true;
+        short num=10000;
+        double num2=2222.0;
+        long o=11111111111111l;
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println( myBool);
+        System.out.println(myText );
+        System.out.println(num1);
+        System.out.println(statement);
+
+        /*   sum of 2 numbers in java
+        int i =10;
+       int q= 90;
+       System.out.println("sum:" + (i+q)); */
+    }
+}
