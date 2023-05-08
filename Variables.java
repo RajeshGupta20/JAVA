c94cca150cc47f4320bebcdb47605f2b605e14bf
RajeshGupta20 <rajgupta2021252gmail.com>
2023-05-08 21:22:33 +0530

    variable

--- Variables.java
+++ Variables.java
@@ -2,9 +2,13 @@ public class Variables {
     public static void main(String[] args) {
         String name = "Rajesh";
         int a=10;
-        int b=9;
-        int c= 100;
+        float val=2.0f;
+        boolean bool=false;
+        char raj='a';
         System.out.println(name);
-        System.out.println(a + b + c);
+        System.out.println(a );
+        System.out.println(val);
+        System.out.println(bool);
+        System.out.println(raj);
     }
 }
